345d93efa5856128767aca6599bc02f2381f6e67
utlandr <rbel068@aucklanduni.ac.nz>
2019-04-09 17:53:23 +1200

    Functioning pipreqs.sh for ipython notebooks

--- pipreqs.sh
+++ pipreqs.sh
@@ -11,7 +11,28 @@
 #  script finds and converts all jupyter notebooks into 
 #  temporary python scripts to get dependency info.
 
+# Provide acticate script
+env_dir="./env"
+source "$env_dir/bin/activate"
 
-#  Find all ipython notebooks by extension
-notebooks=$(find . -type f -name "*.ipynb")
+#  Find all ipython notebooks by extension and ignore hits
+#  in certain directories
+notebooks=$(find ./ -type f -name "*.ipynb" | grep -v "$env_dir" | grep -v "\.\/\.")
 
+mkdir -p "./.tmp"
+
+#  Iterate through python notebooks and make a copy in tmp.
+for ipynb_temp in $notebooks
+do
+	copy_nb=".tmp/copy-$(basename $ipynb_temp)"
+	cp "$ipynb_temp" $copy_nb 
+	jupyter nbconvert --to script $copy_nb
+done
+
+echo Done
+
+# call pipreqs and overwrite into requirements.txt
+pipreqs --force "./" 
+
+# Delete tmp folder
+rm -rf .tmp
